d9a7232d701204b27e9871305c5e6092dd2fcdf6
Keshav Chakravarthy <keshav-c@users.noreply.github.com>
2023-02-25 16:46:06 +0530

    init

---
+++ test/Ballot.ts
@@ -0,0 +1,25 @@
+import { ethers } from "hardhat";
+import { Ballot } from "../typechain-types";
+
+
+describe("Ballot", () => {
+    let ballotContract: Ballot;
+    const PROPOSALS = ["proposal A", "proposal B", "proposal C"];
+    function convertToBytes32(proposal: string[]): string[] {
+        return proposal.map((p) => ethers.utils.formatBytes32String(p));
+    }
+    beforeEach(async () => {
+        const ballotContractFactory = await ethers.getContractFactory("Ballot");
+        ballotContract = await ballotContractFactory.deploy(convertToBytes32(PROPOSALS));
+        console.log("It works!!");
+    });
+    describe("when the contract is deployed", () => {
+        it("has the the provided proposals", async function () { });
+        it("sets the deployer address as the chairperson", async function () { });
+        it("has zero votes for all proposals", async function () { });
+        it("sets the voting weight for the chairperson to 1", async function () { });
+    })
+})
+
+// parse and print the deployed proposals
+// debug and see the proposal values
