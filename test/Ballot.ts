2d482b245c38c98bc0daf4854a7477213c259168
Keshav Chakravarthy <keshav-c@users.noreply.github.com>
2023-02-25 19:37:23 +0530

    test the chairperson identity

--- test/Ballot.ts
+++ test/Ballot.ts
@@ -1,3 +1,4 @@
+import { expect } from "chai";
 import { ethers } from "hardhat";
 import { Ballot } from "../typechain-types";
 
@@ -12,11 +13,16 @@ describe("Ballot", () => {
         const ballotContractFactory = await ethers.getContractFactory("Ballot");
         const convertedProposals = convertToBytes32(PROPOSALS);
         ballotContract = await ballotContractFactory.deploy(convertedProposals);
-        console.log(convertedProposals);
+        const transactionReceipt = await ballotContract.deployTransaction.wait();
     });
     describe("when the contract is deployed", () => {
         it("has the the provided proposals", async function () { });
-        it("sets the deployer address as the chairperson", async function () { });
+        it("sets the deployer address as the chairperson", async function () {
+            const signers = await ethers.getSigners();
+            const deployerAddress = await signers[0].getAddress();
+            const chairperson = await ballotContract.chairperson();
+            expect(chairperson).to.equal(deployerAddress);
+        });
         it("has zero votes for all proposals", async function () { });
         it("sets the voting weight for the chairperson to 1", async function () { });
     })
