2061fbde7e4c2dde8984b7fd4da8b584fd2e3b7e
Keshav Chakravarthy <keshav-c@users.noreply.github.com>
2023-02-26 08:34:00 +0530

    print passed arguments

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -3,6 +3,9 @@ import { ethers } from "hardhat";
 const PROPOSALS = ["Proposal A", "Proposal B", "Proposal C"];
 
 async function main() {
+    const args = process.argv;
+    console.log("Arguments: ", args);
+    return;
     console.log("Deploying Ballot contract");
     console.log("Proposals: ");
     PROPOSALS.forEach((element, index) => {
