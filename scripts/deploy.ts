587b33b41bfb6957a769af0ab6ba836a0ba54d99
Keshav Chakravarthy <keshav-c@users.noreply.github.com>
2023-02-26 08:25:55 +0530

    init deployment script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,23 +1,17 @@
 import { ethers } from "hardhat";
 
+const PROPOSALS = ["Proposal A", "Proposal B", "Proposal C"];
+
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
-
-  const lockedAmount = ethers.utils.parseEther("1");
-
-  const Lock = await ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log(`Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`);
+    console.log("Deploying Ballot contract");
+    console.log("Proposals: ");
+    PROPOSALS.forEach((element, index) => {
+        console.log(`Proposal N. ${index + 1}: ${element}`);
+    });
+    // TODO
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
+    console.error(error);
+    process.exitCode = 1;
 });
